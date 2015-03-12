9c4420c18026b3fc34e3cb998346b45d62b39bb3
Ryan McLean <mclean_ryan@gsb.stanford.edu>
2015-03-12 07:13:32 -0700

    Added rspec to framework.  (RM)

--- Gemfile
+++ Gemfile
@@ -41,6 +41,11 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # Testing from MyOps
+  gem 'rake'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 
 # Clear Heroku warnings
--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,5 +5,13 @@ class EmailProcessor
 
   def process
     # Post.create!({ body: email.body, email: email.from })
+    puts "Email from #{@email.from[:token]}"
+    puts "Routing to account #{@email.to.first[:host].split('.').first}"
+    puts "Command is #{@email.to.first[:token]}"
+    puts "Subject is #{@email.subject}"
+    puts "Headers:"
+    @email.headers.each do |headerName, value|
+      puts "  <#{headerName}> = #{value} "
+    end
   end
 end 
