6b2efafd4e72c663db05561baefa3f8ea41447c1
Ryan McLean <mclean_ryan@gsb.stanford.edu>
2015-03-12 23:30:13 -0700

    Basic test that processing an email adds to the Posts.  (RM)

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,13 +6,16 @@ class EmailProcessor
 
   def process
     # Post.create!({ body: email.body, email: email.from })
+    @headers = @email.headers || []
     puts "Email from #{@email.from[:token]}"
     puts "Routing to account #{@email.to.first[:host].split('.').first}"
     puts "Command is #{@email.to.first[:token]}"
     puts "Subject is #{@email.subject}"
     puts "Headers:"
-    @email.headers.each do |headerName, value|
+    @headers.each do |headerName, value|
       puts "  <#{headerName}> = #{value} "
     end
+
+    Post.create!({ body: @email.body, email: @email.from })
   end
 end 
